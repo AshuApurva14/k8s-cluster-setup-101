13cd58fa6baa9f3a0bed8bd46f4d89b79e0dfeb5
Ashutosh Apurva <apooorva01@gmail.com>
2025-07-31 04:01:17 +0000

    new: add terraform eks cluster creation module

---
+++ genai-eks-demo/eks.md
@@ -0,0 +1,8 @@
+# Provisioning the Amazon EKS cluster
+
+[Eks cluster](./eks_cluster.jpg)
+
+Copy and run the following command in your terminal to point the kubectl CLI to the eks-demo cluster:
+
+`aws eks --region us-west-2 update-kubeconfig --name eks-demo`
+
---
+++ scripts/kubectl_eksctl_install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+   
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+kubectl version --client --output=yaml
+   
+#-------------#--------------------#---------------#-----------#
+
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
